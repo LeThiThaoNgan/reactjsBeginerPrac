abe55cb23c56f4459dd12e10fd7a064e8913afc6
Mina <thithaongan.le@my.jcu.edu.au>
2020-05-24 19:47:45 +0800

    State prac

---
+++ State prac/src/jokeData.js
@@ -0,0 +1,15 @@
+const jokeData = [
+    // {
+    //     id: "1",
+    //     question: "What's your name?",
+    //     answer: "My name is Mina"
+    // },
+    // {
+    //     id: "2",
+    //     question: "How old are you?",
+    //     answer: "I'm 20 year old"
+    // }
+
+]
+
+export default jokeData
